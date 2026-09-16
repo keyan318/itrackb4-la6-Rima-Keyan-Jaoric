42e70efcf2b2f1e9258e8daf5ec709faa0fa47ba
Keyan Jaoric Rima <keyanjaoricrima@Keyans-MacBook-Neo.local>
2026-09-16 17:36:30 +0800

    Updated routes with resource line, custome routes above, restricted to index and show

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
-
+use App\Http\Controllers\TeacherController;
 
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/movies', [MovieController::class, 'index'])->name('movies.index');
-Route::get('/movies/featured', [MovieController::class, 'featured' ])->name('movies.featured');
+//Movies
+Route::get('/movies/featured', [MovieController::class, 'featured'])->name('movies.featured');
 Route::get('/movies/filter/{value?}', [MovieController::class, 'filter'])->name('movies.filter');
-Route::get('/movies/{id}', [MovieController::class, 'show'])->name('movies.show');
+Route::resource('movies', MovieController::class)->only(['index', 'show']);
+
+//Teachers
+Route::get('/teachers/featured', [TeacherController::class, 'featured'])->name('teachers.featured');
+Route::resource('teachers', TeacherController::class);
+Route::resource('teachers', TeacherController::class)->only(['index.show']);
 
 
