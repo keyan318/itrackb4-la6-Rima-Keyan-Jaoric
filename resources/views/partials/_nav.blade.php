d01664dcd89568339dae894d3614a482bfa3c907
Keyan Jaoric Rima <keyanjaoricrima@Keyans-MacBook-Neo.local>
2026-09-23 19:45:54 +0800

    Mark active nav link using wildcard path check for movies

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="nav">
-    <a class="nav-link" href="{{route('movies.index')}}">All Movies</a>
-    <a class="nav-link" href="{{route('movies.featured')}}">Featured</a>
-    <a class="nav-link" href="{{route('movies.filter')}}">Filter</a>
-
+    <a class="nav-link {{ request()->is('movies*') && !request()->is('movies/featured') ? 'active fw-bold' : '' }}"
+       href="{{route('movies.index')}}">All Movies</a>
+    <a class="nav-link {{ request()->is('movies/featured') ? 'active fw-bold' : '' }}"
+       href="{{route('movies.featured')}}">Featured</a>
 </nav>
